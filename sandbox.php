c7c4b77bd64d506c70d6c07a3b96fdf9f1a0a422
Navika-M <47578845+Navika-M@users.noreply.github.com>
2020-09-17 15:16:36 -0600

    classes and objects

--- sandbox.php
+++ sandbox.php
@@ -1,7 +1,49 @@
-<?php 
+<?php
 
-// file system - part 1
+// classes
 
-$quotes = readfile
+class User{
+
+    private $email;
+    private $name;
+
+    public function __construct($name, $email)
+    {
+        // $this -> email = 'mario@thenetninja.com';
+        // $this -> name = 'mario';        
+        $this->email = $email;
+        $this->name = $name;
+    }
+
+    public function login(){
+        // echo 'the user logged in';
+        echo $this-> name . ' logged in'; 
+    }
+
+    public function getName(){
+        return $this-> name;
+    }
+
+    public function setName($name){
+        if(is_string($name) && strlen($name) > 1){
+            $this-> name = $name;
+            return "name has been updated to $name";
+        } else{
+            return 'not a valid name';
+        }
+    }
+}
+
+
+
+
+$userTwo = new User('yoshi', 'yoshi@thenetninja.com');
+
+//echo $userTwo-> getName();
+// echo $userTwo-> setName(50);
+echo $userTwo-> setName('shaun');
+echo $userTwo->getName();
+
+
 
 ?>
